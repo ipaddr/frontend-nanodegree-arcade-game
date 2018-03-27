caf29653df2bbf0a16cc82b6fa308a4b10e3bebd
i_p_addr@yahoo.com <i_p_addr@yahoo.com>
2018-03-27 17:57:57 +0700

    add some tune

--- js/app.js
+++ js/app.js
@@ -1,38 +1,64 @@
-const enemyHeight = 25;
-const enemyWidth = 60;
-const enemyDefaultSpeed = 100;
-const playerHeight = 30;
-const playerWidth = 50;
-const playerDefaultX = 200;
-const playerDefaultY = 380;
-const playerDefaultSpeed = 50;
-const canvasWidth = 505;
-const canvasHeight = 606;
-const canvasLefBoundary = 0;
-const canvasRightBoundary = 400;
+// Whole-script strict mode syntax
+'use strict';
+
+const ENEMY_HEIGHT = 25;
+const ENEMY_WIDTH = 60;
+const ENEMY_DEFAULT_SPEED = 100;
+const PLAYER_HEIGHT = 30;
+const PLAYER_WIDTH = 50;
+const PLAYER_DEFAULT_X = 200;
+const PLAYER_DEFAULT_Y = 380;
+const PLAYER_DEFAULT_SPEED = 50;
+const CANVAS_WIDTH = 505;
+const CANVAS_HEIGHT = 606;
+const CANVAS_TOP_BOUNDARY = 0;
+const CANVAS_LEF_BOUNDARY = 0;
+const CANVAS_RIGHT_BOUNDARY = 400;
+
+
+// parent class for enemy and player
+function ParentClass(x, y, speed){
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+}
+
+// Draw the enemy on the screen, required method for game
+ParentClass.prototype.render = function(){
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+// Detect isCollision between enemy and player
+ParentClass.prototype.isCollision = function(objectA, objectB){
+    if(objectA.x + objectA.width >= objectB.x &&
+        objectA.x <= objectB.x + objectB.width &&
+        objectA.y + objectA.height >= objectB.y &&
+        objectA.y <= objectB.y + objectB.height)
+        return true;
+    return false;
+}
 
 
 // Enemies our player must avoid
-var Enemy = function(x, y, speed) {
+const Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+
+    // position of the enemie and speed of the enemie
+    ParentClass.call(this, x,y,speed);
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
 
     // width and height of object enemi
-    this.width = enemyWidth;
-    this.height = enemyHeight;
-
-    // position of the enemie
-    this.x = x;
-    this.y = y;
-
-    // speed of the enemie
-    this.speed = speed;
+    this.width = ENEMY_WIDTH;
+    this.height = ENEMY_HEIGHT;
 
 };
+
+// enemy class extends to Parent class
+Enemy.prototype = Object.create(ParentClass.prototype);
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -45,68 +71,54 @@ Enemy.prototype.update = function(dt) {
     this.x = this.x + (this.speed * dt);
 
     // if enemies arrive at the end of field
-    if (this.x > canvasWidth) {
+    if (this.x > CANVAS_WIDTH) {
         this.x = 0;
-        this.speed = enemyDefaultSpeed + Math.floor(Math.random() * 512);
+        this.speed = ENEMY_DEFAULT_SPEED + Math.floor(Math.random() * 512);
     }
 
     // Handles collision with the Player (you need to implement)
-    if(isCollision(this, player))
+    if(this.isCollision(this, player))
         player.resetPosition();
 
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-class Player{
+class Player extends ParentClass{
 
     constructor(x, y, speed){
         // Variables applied to each of our instances go here,
         // we've provided one for you to get started
+
+        // position of the enemie and speed of the enemie
+        super(x, y, speed);
 
         // The image/sprite for our enemies, this uses
         // a helper we've provided to easily load images
         this.sprite = 'images/char-boy.png';
 
         // width and height of object enemi
-        this.width = playerWidth;
-        this.height = playerHeight;
-
-        // position of the enemie
-        this.x = x;
-        this.y = y;
-
-        // speed of the enemie
-        this.speed = playerDefaultSpeed;
+        this.width = PLAYER_WIDTH;
+        this.height = PLAYER_HEIGHT;
     }
 
     // update player position and condition if hit boundary
     update(){
 
-        if(this.y < 0){
+        if(this.y < CANVAS_TOP_BOUNDARY){
             this.resetPosition();
             alert('You won!');
         }
-        else if (this.y > playerDefaultY){
-            this.y = playerDefaultY;
+        else if (this.y > PLAYER_DEFAULT_Y){
+            this.y = PLAYER_DEFAULT_Y;
         }
-        else if (this.x > canvasRightBoundary){
-            this.x = canvasRightBoundary
+        else if (this.x > CANVAS_RIGHT_BOUNDARY){
+            this.x = CANVAS_RIGHT_BOUNDARY
         }
-        else if (this.x < 0){
-            this.x = 0;
+        else if (this.x < CANVAS_LEF_BOUNDARY){
+            this.x = CANVAS_LEF_BOUNDARY;
         }
-    }
-
-    // render the player
-    render(){
-        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 
     // handling keyboard input, as lef, up, right, down
@@ -130,8 +142,8 @@ class Player{
 
     // reset postion to defautl position
     resetPosition(){
-        this.x = playerDefaultX;
-        this.y = playerDefaultY;
+        this.x = PLAYER_DEFAULT_X;
+        this.y = PLAYER_DEFAULT_Y;
     }
 
 }
@@ -143,10 +155,10 @@ class Player{
 
 let allEnemies = [];
 let enemyPosition = [60, 140, 220];
-let player = new Player(playerDefaultX, playerDefaultY, playerDefaultSpeed);
+let player = new Player(PLAYER_DEFAULT_X, PLAYER_DEFAULT_Y, PLAYER_DEFAULT_SPEED);
 
 enemyPosition.forEach(function(positionY) {
-    const enemy = new Enemy(0, positionY, enemyDefaultSpeed + Math.floor(Math.random() * 512));
+    const enemy = new Enemy(0, positionY, ENEMY_DEFAULT_SPEED + Math.floor(Math.random() * 512));
     allEnemies.push(enemy);
 });
 
@@ -163,14 +175,3 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
-
-
-function isCollision(objectA, objectB){
-    if(objectA.x + objectA.width >= objectB.x &&
-        objectA.x <= objectB.x + objectB.width &&
-        objectA.y + objectA.height >= objectB.y &&
-        objectA.y <= objectB.y + objectB.height)
-        return true;
-    return false;
-
-}
